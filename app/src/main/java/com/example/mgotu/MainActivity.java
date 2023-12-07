bcaa58f0ca1410f949a368a4f7968f2ae9b8a81a
danya201272 <36302863+danya201272@users.noreply.github.com>
2023-12-07 04:12:22 +0300

    Add Camera

--- app/src/main/java/com/example/mgotu/MainActivity.java
+++ app/src/main/java/com/example/mgotu/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.DownloadManager;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.net.http.SslError;
 import android.os.Build;
@@ -11,6 +12,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.KeyEvent;
 import android.webkit.CookieManager;
+import android.webkit.PermissionRequest;
 import android.webkit.SslErrorHandler;
 import android.webkit.URLUtil;
 import android.webkit.ValueCallback;
@@ -20,8 +22,8 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,18 +34,44 @@ public class MainActivity extends AppCompatActivity {
     private final static int FILECHOOSER_RESULTCODE = 1;
     SwipeRefreshLayout swipeRefreshLayout;
     String url = "https://ies.unitech-mo.ru/schedule";
-
+    String[] permissions={
+            "android.permission.ACCESS_DOWNLOAD_MANAGER",
+            "android.permission.WRITE_EXTERNAL_STORAGE",
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.READ_MEDIA_IMAGES",
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.CAMERA"};
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (requestCode == REQUEST_SELECT_FILE) {
+                if (uploadMessage == null)
+                    return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessage = null;
+            }
+        } else if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage)
+                return;
+            Uri result = intent == null || resultCode != MainActivity.RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        } else
+            Toast.makeText(getApplicationContext(), "Ошибка: Загрузки Фото", Toast.LENGTH_LONG).show();
+    }
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkDownloadPermission();
+        requestPermissions(permissions,80);
         CookieManager.getInstance().setAcceptCookie(true);
 
         webView = findViewById(R.id.web);
         swipeRefreshLayout = findViewById(R.id.swipe);
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         webView.getSettings().setSupportZoom(false);
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setAllowFileAccessFromFileURLs(true);
@@ -52,15 +80,23 @@ public class MainActivity extends AppCompatActivity {
 
         swipeRefreshLayout.setEnabled(false); // Delete если надо свайп
         swipeRefreshLayout.setRefreshing(false); // Delete если надо свайп
-        //swipeRefreshLayout.setOnRefreshListener(() -> {
-        //swipeRefreshLayout.setRefreshing(true);
-        //new Handler().postDelayed(() -> {
-        //swipeRefreshLayout.setRefreshing(false);
-        //webView.reload();
-        //},  3000);
-        //});
+        /*
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+        swipeRefreshLayout.setRefreshing(true);
+        new Handler().postDelayed(() -> {
+        swipeRefreshLayout.setRefreshing(false);
+        webView.reload();
+        },  3000);
+        });
+        */
         webView.setWebChromeClient(new WebChromeClient()
         {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    request.grant(request.getResources());
+                }
+            }
             // For Lollipop 5.0+ Devices
             public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams)
             {
@@ -71,19 +107,26 @@ public class MainActivity extends AppCompatActivity {
 
                 uploadMessage = filePathCallback;
 
-                Intent intent = null;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                    intent = fileChooserParams.createIntent();
-                }
+                Intent intent = fileChooserParams.createIntent();
                 try
                 {
                     startActivityForResult(intent, REQUEST_SELECT_FILE);
                 } catch (ActivityNotFoundException e)
                 {
                     uploadMessage = null;
+                    Toast.makeText(getApplicationContext(), "Ошибка: Открытия проводника", Toast.LENGTH_LONG).show();
                     return false;
                 }
                 return true;
+            }
+            //For Android 4.1 only
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture)
+            {
+                mUploadMessage = uploadMsg;
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Проводник"), FILECHOOSER_RESULTCODE);
             }
             protected void openFileChooser(ValueCallback<Uri> uploadMsg)
             {
@@ -91,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(Intent.ACTION_GET_CONTENT);
                 i.addCategory(Intent.CATEGORY_OPENABLE);
                 i.setType("image/*");
-                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+                startActivityForResult(Intent.createChooser(i, "Проводник"), FILECHOOSER_RESULTCODE);
             }
         });
         webView.setDownloadListener((url, userAgent, contentDisposition, mimetype, contentLength) -> {
@@ -105,11 +148,12 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Скачивание...", Toast.LENGTH_SHORT).show();
         });
     }
-    public static class myWebViewclient extends WebViewClient{
+    public class myWebViewclient extends WebViewClient{
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
             view.loadUrl(request.getUrl().toString());
             CookieManager.getInstance().flush();
+            requestPermissions(permissions,80);
             return true;
         }
         @Override
@@ -128,36 +172,24 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onKeyDown(keyCode, event);
     }
+    /*
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            if (requestCode == REQUEST_SELECT_FILE) {
-                if (uploadMessage == null)
-                    return;
-                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
-                uploadMessage = null;
-            }
-        } else if (requestCode == FILECHOOSER_RESULTCODE) {
-            if (null == mUploadMessage)
-                return;
-            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
-            // Use RESULT_OK only if you're implementing WebView inside an Activity
-            Uri result = intent == null || resultCode != MainActivity.RESULT_OK ? null : intent.getData();
-            mUploadMessage.onReceiveValue(result);
-            mUploadMessage = null;
-        }
+    protected void onResume(){
+       super.onResume();
+       webView.reload();
     }
-    //@Override
-    //protected void onResume(){
-       // super.onResume();
-       // webView.reload();
-    //}
-    private void checkDownloadPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            Toast.makeText(MainActivity.this, "Дайте разрешение к файлам. В настройках приложения.", Toast.LENGTH_LONG).show();
-        } else {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+    */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==80)
+        {
+            if (grantResults[0]== PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this,"Права предоставлены",Toast.LENGTH_SHORT).show();
+            }
+            /* else {
+                Toast.makeText(this,"Ошибка прав",Toast.LENGTH_SHORT).show();
+            }*/
         }
     }
 }
